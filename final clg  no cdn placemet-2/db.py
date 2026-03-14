8844fc38c873000b94f1b88afce6552ab4f739e6
Balaji Nagmoti <balajinagmotibca01@gmail.com>
2026-03-14 22:58:56 +0530

    Add files via upload

---
+++ final clg  no cdn placemet-2/db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+def get_db_connection():
+    try:
+     
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='mysql', 
+            database='tryplacement' 
+        )
+        return connection
+    except Exception as e:
+        print(f"Database error: {e}")
+        return None
